595a191219c3c97cec8790a9ff19733e8c996bbe
mnathava <mnathava@qti.qualcomm.com>
2025-04-29 06:45:58 +0000

    made changes to a comment

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 00_03 with modifications
+// Exercise 00_03 with modifications and more modifications
 // Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 
 #include <iostream>
