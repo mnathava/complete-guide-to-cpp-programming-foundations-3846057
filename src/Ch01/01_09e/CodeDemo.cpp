9c642634b7e1b0327dd3e0b3e0b1af2b0f7d9fcd
mnathava <mnathava@qti.qualcomm.com>
2025-04-29 07:59:17 +0000

    program now takes in name and outputs it

--- src/Ch01/01_09e/CodeDemo.cpp
+++ src/Ch01/01_09e/CodeDemo.cpp
@@ -7,10 +7,10 @@
 
 int main(){
     std::string name;
-    std::cout << "Enter your name: " << std::flush;
-    std::cin >> name;
+    std:: cout << "Please enter your name: " << std::flush;
+    std:: cin >> name;
     std::cout << "Nice to meet you, " << name << "!" << std::endl;
+    
 
-    std::cout << std::endl << std::endl;
     return 0;
 }
